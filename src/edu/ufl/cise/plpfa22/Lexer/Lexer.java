473ccdd6fde7f954e63c5180fcf8a8b3394a0b24
d3v-26 <68773259+d3v-26@users.noreply.github.com>
2022-09-11 13:57:45 -0400

    Update Lexer.java

--- src/edu/ufl/cise/plpfa22/Lexer/Lexer.java
+++ src/edu/ufl/cise/plpfa22/Lexer/Lexer.java
@@ -1,9 +1,49 @@
 package edu.ufl.cise.plpfa22.Lexer;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import edu.ufl.cise.plpfa22.Lexer.IToken.Kind;
+import edu.ufl.cise.plpfa22.Lexer.IToken.SourceLocation;
+
+/**
+ * 
+ * @author Dev's PC
+ * LEXER Class has following Constructors:
+ * 	- Lexer(String)
+ * 
+ * LEXER Class has following methods:
+ * 	- IToken next()
+ *  - IToken peek()  
+ */
+
+
 public class Lexer implements ILexer {
 
+	public String input;
+	public int pos, line;
+	public List<String> lines;
+	
+	public enum States{
+		START,
+		HAVE_GT,
+		HAVE_LT,
+		HAVE_QUOTE,
+		HAVE_QUOTE_SLASH,
+		HAVE_UNQUOTE,
+		HAVE_COLON,
+		HAVE_EQUALS,
+		GE, LE, NEQ,
+		IDENT, KW, BOOL_IDENT
+	}
+	
 	public Lexer(String input) {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor `stub
+		this.pos = 0;
+		this.line = 0;
+		this.input = input;
+		this.lines = input.lines().toList();
 	}
 
 	@Override

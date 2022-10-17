a31bb9867da84cbec02388cc8d5124ba06222d58
Dharmam Savani <savanidharmam5@gmail.com>
2022-10-17 14:12:57 -0400

    Update ASTVisitorImpl.java

--- src/edu/ufl/cise/plpfa22/ASTVisitorImpl.java
+++ src/edu/ufl/cise/plpfa22/ASTVisitorImpl.java
@@ -4,6 +4,7 @@ import edu.ufl.cise.plpfa22.IToken.Kind;
 import edu.ufl.cise.plpfa22.ast.ASTVisitor;
 import edu.ufl.cise.plpfa22.ast.Block;
 import edu.ufl.cise.plpfa22.ast.ConstDec;
+import edu.ufl.cise.plpfa22.ast.Declaration;
 import edu.ufl.cise.plpfa22.ast.ExpressionBinary;
 import edu.ufl.cise.plpfa22.ast.ExpressionBooleanLit;
 import edu.ufl.cise.plpfa22.ast.ExpressionIdent;
@@ -21,6 +22,7 @@ import edu.ufl.cise.plpfa22.ast.StatementIf;
 import edu.ufl.cise.plpfa22.ast.StatementInput;
 import edu.ufl.cise.plpfa22.ast.StatementOutput;
 import edu.ufl.cise.plpfa22.ast.StatementWhile;
+import edu.ufl.cise.plpfa22.ast.Types.Type;
 import edu.ufl.cise.plpfa22.ast.VarDec;
 
 public class ASTVisitorImpl implements ASTVisitor {
@@ -171,85 +173,61 @@ public class ASTVisitorImpl implements ASTVisitor {
 	@Override
 	public Object visitExpressionBinary(ExpressionBinary expressionBinary, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
-		int left = (Integer) expressionBinary.e0.visit(this, arg);
-		int right = (Integer) expressionBinary.e1.visit(this, arg);
-		IToken op = expressionBinary.op;
-		switch(op.getKind()) {
-			case PLUS -> {
-				return left + right;
-			}
-			case MINUS -> {
-				return left - right;
-			}
-			case TIMES -> {
-				return left * right;
-			}
-			case DIV -> {
-				if(right == 0) {
-					throw new SyntaxException();
-				}
-				return left / right;
-			}
-			case MOD -> {
-				return left % right;
-			}
-			case LT -> {
-				return left < right;
-			}
-			case GT -> {
-				return left > right;
-			}
-			case LE -> {
-				return left <= right;
-			}
-			case GE -> {
-				return left >= right;
-			}
-			case EQ -> {
-				return left == right;
-			}
-			case NEQ -> {
-				return left != right;
-			}
-			default -> {
-				throw new SyntaxException();
-			}
-		}
+		expressionBinary.e0.visit(this, arg);
+		expressionBinary.e1.visit(this, arg);
+		return null;
 	}
 
 	@Override
 	public Object visitExpressionIdent(ExpressionIdent expressionIdent, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
-		return expressionIdent.firstToken.getStringValue();
+		IToken id = expressionIdent.firstToken;
+		expressionIdent.setNest(this.symbolTable.scopeStack.peek());
+		Declaration d = this.symbolTable.lookUp(String.valueOf(id.getText()));
+		if (d == null) {
+			throw new ScopeException(String.valueOf(id.getText()));
+		}
+		expressionIdent.setDec(d);
+		return null;
 	}
 
 	@Override
 	public Object visitExpressionNumLit(ExpressionNumLit expressionNumLit, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
+		expressionNumLit.setType(Type.NUMBER);
 		return expressionNumLit.firstToken.getIntValue();
 	}
 
 	@Override
 	public Object visitExpressionStringLit(ExpressionStringLit expressionStringLit, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
+		expressionStringLit.setType(Type.STRING);
 		return expressionStringLit.firstToken.getStringValue();
 	}
 
 	@Override
 	public Object visitExpressionBooleanLit(ExpressionBooleanLit expressionBooleanLit, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
+		expressionBooleanLit.setType(Type.BOOLEAN);
 		return expressionBooleanLit.firstToken.getBooleanValue();
 	}
 
 	@Override
 	public Object visitProcedure(ProcDec procDec, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
-		return procDec.block.visit(this, arg);
+		checkNotDecIdent(String.valueOf(procDec.ident.getText()));
+		procDec.setNest(this.symbolTable.scopeStack.peek());
+		procDec.setType(Type.PROCEDURE);
+		this.symbolTable.insert(String.valueOf(procDec.ident.getText()), procDec);
+		return null;
 	}
 
 	@Override
 	public Object visitConstDec(ConstDec constDec, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
+		checkNotDecIdent(String.valueOf(constDec.ident.getText()));
+		constDec.setNest(this.symbolTable.scopeStack.peek());
+		this.symbolTable.insert(String.valueOf(constDec.ident.getText()), constDec);
 		return null;
 	}
 
@@ -262,6 +240,13 @@ public class ASTVisitorImpl implements ASTVisitor {
 	@Override
 	public Object visitIdent(Ident ident, Object arg) throws PLPException {
 		// TODO Auto-generated method stub
+		checkDecIdent(String.valueOf(ident.firstToken.getText()));
+		ident.setNest(this.symbolTable.scopeStack.peek());
+		Declaration d = this.symbolTable.lookUp(String.valueOf(ident.getText()));
+		if (d == null) {
+			throw new ScopeException(String.valueOf(ident.getText()));
+		}
+		ident.setDec(d);
 		return null;
 	}
 
